5afa18bafcc115a0512613ce8d5777ead5564398
d-kondratenko <dmitkondratenko@gmail.com>
2022-09-19 10:00:47 +0300

    create project

---
+++ src/main/java/com/epic/invoicecheck/InvoicecheckApplication.java
@@ -0,0 +1,13 @@
+package com.epic.invoicecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicecheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicecheckApplication.class, args);
+	}
+
+}
