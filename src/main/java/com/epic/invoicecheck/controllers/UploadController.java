1796b901a9cd9d39d7f8de5cdf01ff3465103276
d-kondratenko <dmitkondratenko@gmail.com>
2022-10-18 14:40:53 +0300

    add new class UploadMessage add this class to model attribute and to page

--- src/main/java/com/epic/invoicecheck/controllers/UploadController.java
+++ src/main/java/com/epic/invoicecheck/controllers/UploadController.java
@@ -2,6 +2,7 @@ package com.epic.invoicecheck.controllers;
 
 import com.epic.invoicecheck.entity.DeliveryInfo;
 import com.epic.invoicecheck.models.UploadForm;
+import com.epic.invoicecheck.models.UploadMessage;
 import com.epic.invoicecheck.repository.DeliveryInfoRepository;
 import com.epic.invoicecheck.repository.DifferentsRepository;
 import com.epic.invoicecheck.services.CheckService;
@@ -30,6 +31,7 @@ public class UploadController {
     @GetMapping
     public String upload(Model model){
         model.addAttribute("uploadform", new UploadForm());
+        model.addAttribute("msg", new UploadMessage());
         return "uploadform";
     }
 
@@ -37,13 +39,14 @@ public class UploadController {
     public Object postUpload(@RequestParam("file") MultipartFile file, @ModelAttribute UploadForm uploadForm, Model model) throws IOException, JRException {
         if (checkService.startChecking(file, uploadForm).isEmpty()) {
             model.addAttribute("uploadform", new UploadForm());
+            model.addAttribute("msg", new UploadMessage("No difference between order and invoice"));
             return "uploadform";
         } else {
             DeliveryInfo delInfo = deliveryInfoRepository.findByOrderNrAndSupplireNr(uploadForm.getOrderNr(), uploadForm.getSupplireNr());
             RedirectView redirectView = new RedirectView();
             log.info("redirect");
-            redirectView.setUrl("/invoicecheck/report/" + delInfo.getDeliveryId());
-           // redirectView.setUrl("/report/" + delInfo.getDeliveryId());
+            // redirectView.setUrl("/invoicecheck/report/" + delInfo.getDeliveryId());
+            redirectView.setUrl("/report/" + delInfo.getDeliveryId());
             log.info(redirectView.getUrl());
             log.info("set url");
             return redirectView;
---
+++ src/main/java/com/epic/invoicecheck/models/UploadMessage.java
@@ -0,0 +1,12 @@
+package com.epic.invoicecheck.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadMessage {
+    private String msg;
+}
